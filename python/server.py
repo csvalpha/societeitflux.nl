562b0dfdfc5c6d0e482727517e0f28a28baeba5d
P. Somhorst <peter.somhorst@gmail.com>
2013-12-16 12:36:27 +0100

    Updated server, updated rental form.

--- python/server.py
+++ python/server.py
@@ -1,4 +1,6 @@
-from bottle import Bottle, run, static_file, redirect, request
+from bottle import Bottle, run, static_file, redirect, request, template
+import smtplib
+from email.mime.text import MIMEText
 
 app = Bottle()
 
@@ -6,12 +8,39 @@ app = Bottle()
 def index():
 	redirect('/index.html')
 
+@app.route('/sendform',method="GET")
+def sendformGet():
+	redirect('/index.html')
+
 @app.route('/sendform',method="POST")
 def sendform():
-	print request.forms.get('addressStreet')
+	print request.POST.get('name.first')
+
+
+	# Import the email modules we'll need
+
+
+	# Open a plain text file for reading.  For this example, assume that
+	# the text file contains only ASCII characters.
+	# Create a text/plain message
+	msg = MIMEText("TEST")
+
+	me = request.POST.get('email')
+	you = "peter.somhorst@gmail.com"
+	msg['Subject'] = 'Dit is een testmail'
+	msg['From'] = me
+	msg['To'] = you
+
+	# Send the message via our own SMTP server, but don't include the
+	# envelope header.
+	s = smtplib.SMTP('localhost')
+	s.sendmail(me, [you], msg.as_string())
+	s.quit()
+
+
 
 @app.route('/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root="../")
+		return static_file(filepath, root="/home/flux/website/front")
 
-run(app,host='vivaan.csvalpha.nl', port=8080, debug=True)
+run(app,host='localhost', port=8080, debug=True)
